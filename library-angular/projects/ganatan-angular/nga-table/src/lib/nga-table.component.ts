b72c5a9d7743fe80cf6d5c9cda75c3b47ff03720
danny.collodet <danny.collodet.external@airbus.com>
2025-02-07 10:56:13 +0100

    improve architecture

---
+++ library-angular/projects/ganatan-angular/nga-table/src/lib/nga-table.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nga-table',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      nga-table works! test
+    </p>
+  `,
+  styles: ``
+})
+export class NgaTableComponent {
+
+}

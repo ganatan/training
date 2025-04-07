9263fdc99a5ff03d7219f605fa4f6075edb47e78
ganatan <dannyganatan@gmail.com>
2025-04-07 06:44:30 +0200

    Update dependencies on backend-nodejs & frontend-angular

--- backend-nodejs/03-backend-nodejs/src/shared/generic/base.controller.js
+++ backend-nodejs/03-backend-nodejs/src/shared/generic/base.controller.js
@@ -16,6 +16,7 @@ class BaseController {
   async getItems(req, res, next) {
     try {
       const result = await this.service.getItems(req.query);
+
       return res.status(HTTP_STATUS.OK).json(result);
     } catch (error) {
       return next(error);
@@ -26,11 +27,13 @@ class BaseController {
     try {
       const id = parseInt(req.params.id);
       const result = await this.service.getItemById(id);
+
       return res.status(HTTP_STATUS.OK).json(result);
     } catch (error) {
       if (error.message === this.constants.NOT_FOUND) {
         return next({ status: HTTP_STATUS.NOT_FOUND, message: error.message });
       }
+
       return next(error);
     }
   }
@@ -42,6 +45,7 @@ class BaseController {
       }
 
       const result = await this.service.createItem(req.body);
+
       return res.status(HTTP_STATUS.CREATED).json(result);
     } catch (error) {
       if (error.message === this.constants.ALREADY_EXISTS) {
@@ -50,6 +54,7 @@ class BaseController {
       if (error.name === 'ValidationError') {
         return next({ status: HTTP_STATUS.BAD_REQUEST, message: error.message });
       }
+
       return next(error);
     }
   }
@@ -62,6 +67,7 @@ class BaseController {
 
       const id = parseInt(req.params.id);
       const result = await this.service.updateItem(id, req.body);
+
       return res.status(HTTP_STATUS.OK).json(result);
     } catch (error) {
       if (error.message === this.constants.NOT_FOUND) {
@@ -70,6 +76,7 @@ class BaseController {
       if (error.name === 'ValidationError') {
         return next({ status: HTTP_STATUS.BAD_REQUEST, message: error.message });
       }
+
       return next(error);
     }
   }
@@ -78,107 +85,16 @@ class BaseController {
     try {
       const id = parseInt(req.params.id);
       const result = await this.service.deleteItem(id);
+
       return res.status(HTTP_STATUS.OK).json(result);
     } catch (error) {
       if (error.message === this.constants.NOT_FOUND) {
         return next({ status: HTTP_STATUS.NOT_FOUND, message: error.message });
       }
+
       return next(error);
     }
   }
 }
 
 export default BaseController;
-
-
-
-// import { HTTP_STATUS } from '../constants/http-status.js';
-
-// class BaseController {
-//   constructor(service, constants = {}) {
-//     this.service = service;
-//     this.constants = constants;
-
-//     this.getItems = this.getItems.bind(this);
-//     this.getItemById = this.getItemById.bind(this);
-//     this.createItem = this.createItem.bind(this);
-//     this.updateItem = this.updateItem.bind(this);
-//     this.deleteItem = this.deleteItem.bind(this);
-//   }
-
-//   async getItems(req, res, next) {
-//     try {
-//       const result = await this.service.getItems(req.query);
-
-//       return res.status(HTTP_STATUS.OK).json(result);
-//     } catch (error) {
-//       return next(error);
-//     }
-//   }
-
-//   async getItemById(req, res, next) {
-//     try {
-//       const id = parseInt(req.params.id);
-//       const result = await this.service.getItemById(id);
-
-//       if (!result) {
-//         return next({
-//           status: HTTP_STATUS.NOT_FOUND,
-//           message: this.constants.ITEM_NOT_FOUND || 'Item not found',
-//         });
-//       }
-
-//       return res.status(HTTP_STATUS.OK).json(result);
-//     } catch (error) {
-//       return next(error);
-//     }
-//   }
-
-//   async createItem(req, res, next) {
-//     try {
-//       const result = await this.service.createItem(req.body);
-
-//       return res.status(HTTP_STATUS.CREATED).json(result);
-//     } catch (error) {
-//       return next(error);
-//     }
-//   }
-
-//   async updateItem(req, res, next) {
-//     try {
-//       const id = parseInt(req.params.id);
-//       const result = await this.service.updateItem(id, req.body);
-
-//       if (!result) {
-//         return next({
-//           status: HTTP_STATUS.NOT_FOUND,
-//           message: this.constants.ITEM_NOT_FOUND || 'Item not found',
-//         });
-//       }
-
-//       return res.status(HTTP_STATUS.OK).json(result);
-//     } catch (error) {
-//       return next(error);
-//     }
-//   }
-
-//   async deleteItem(req, res, next) {
-//     try {
-//       const id = parseInt(req.params.id);
-//       const result = await this.service.deleteItem(id);
-
-//       if (!result) {
-//         return next({
-//           status: HTTP_STATUS.NOT_FOUND,
-//           message: this.constants.ITEM_NOT_FOUND || 'Item not found',
-//         });
-//       }
-
-//       return res.status(HTTP_STATUS.OK).json(result);
-//     } catch (error) {
-//       return next(error);
-//     }
-//   }
-// }
-
-// export default BaseController;
--- backend-nodejs/03-backend-nodejs/src/shared/generic/base.service.js
+++ backend-nodejs/03-backend-nodejs/src/shared/generic/base.service.js
@@ -13,6 +13,7 @@ class BaseService {
     if (!item) {
       throw new Error(this.constants.NOT_FOUND || 'Not found');
     }
+
     return item;
   }
 
@@ -44,33 +45,3 @@ class BaseService {
 }
 
 export default BaseService;
-
-
-
-// class BaseService {
-//   constructor(repository) {
-//     this.repository = repository;
-//   }
-
-//   async getItems(query) {
-//     return this.repository.findAll(query);
-//   }
-
-//   async getItemById(id) {
-//     return this.repository.findOne(id);
-//   }
-
-//   async createItem(data) {
-//     return this.repository.create(data);
-//   }
-
-//   async updateItem(id, data) {
-//     return this.repository.update(id, data);
-//   }
-
-//   async deleteItem(id) {
-//     return this.repository.delete(id);
-//   }
-// }
-
-// export default BaseService;

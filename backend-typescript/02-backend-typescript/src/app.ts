27bc6fb33cbf3e26a24fb77def21160bac666f39
ganatan <dannyganatan@gmail.com>
2025-04-15 10:50:30 +0200

    Add Winston on backend-typescript

--- backend-typescript/02-backend-typescript/src/app.ts
+++ backend-typescript/02-backend-typescript/src/app.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import cors from 'cors';
 
 import initLocals from './infrastructure/middleware/core/init-locals';
-
 import notFoundHandler from './infrastructure/middleware/error/not-found-handler';
 import responseHandler from './infrastructure/middleware/error/response-handler';
 import errorHandler from './infrastructure/middleware/error/error-handler';
+
+import requestLogger from './infrastructure/logger/request-logger';
 
 import rootRouter from './routers/root.router';
 import modulesRouter from './routers/modules.router';
@@ -15,6 +16,8 @@ const app = express();
 app.use(cors());
 
 app.use(initLocals);
+
+app.use(requestLogger);
 
 app.use(rootRouter);
 app.use(modulesRouter);

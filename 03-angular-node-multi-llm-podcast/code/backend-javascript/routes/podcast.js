3c65349b34d02f3e8892000bbed4aa0ac48165f4
ganatan <dannyganatan@gmail.com>
2025-05-06 16:22:50 +0200

    Improve angular-node-multi-llm-podcast

--- 03-angular-node-multi-llm-podcast/code/backend-javascript/routes/podcast.js
+++ 03-angular-node-multi-llm-podcast/code/backend-javascript/routes/podcast.js
@@ -1,25 +1,43 @@
 const express = require('express')
 const router = express.Router()
-const { sendToChatGPT } = require('../services/openaiService')
-const { sendToClaude } = require('../services/claudeService')
+const { saveConversationToFile } = require('../services/conversationService')
+const { saveMockConversationToFile } = require('../services/conversationService.mock')
 
-router.get('/start', async (req, res) => {
-  const result = []
+const { generateAllAudioFromJson } = require('../services/audioService')
 
-  let message = "Bonjour Claude, que penses-tu de l'intelligence artificielle en éducation ?"
-  result.push({ speaker: 'ChatGPT', message })
-
-  for (let i = 0; i < 5; i++) {
-    const replyClaude = await sendToClaude(message)
-    result.push({ speaker: 'Claude', message: replyClaude })
-
-    const replyGPT = await sendToChatGPT(replyClaude)
-    result.push({ speaker: 'ChatGPT', message: replyGPT })
-
-    message = replyGPT
+router.get('/generate', async (req, res) => {
+  // const debat = req.query.topic || "Angular ou React : quel framework offre la meilleure architecture pour des applications à grande échelle ?"
+  const debat = req.query.topic || "Dune de Denis Villeneuve : chef-d'œuvre de science-fiction ou exercice de style surcoté ?"
+  try {
+    const { filename, conversation } = await saveConversationToFile(debat)
+    res.json({ success: true, file: filename, conversation })
+  } catch (e) {
+    console.error('Erreur génération podcast:', e)
+    res.status(500).json({ success: false, error: e.message })
   }
+})
 
-  res.json(result)
+router.get('/mock', async (req, res) => {
+  // const debat = req.query.topic || "Angular ou React : quel framework offre la meilleure architecture pour des applications à grande échelle ?"
+  const debat = req.query.topic || "Dune de Denis Villeneuve : chef-d'œuvre de science-fiction ou exercice de style surcoté ?"
+  try {
+    const { filename, conversation } = await saveMockConversationToFile(debat)
+    res.json({ success: true, file: filename, conversation })
+  } catch (e) {
+    console.error('Erreur mock podcast:', e)
+    res.status(500).json({ success: false, error: e.message })
+  }
+})
+
+router.get('/audio/:filename', async (req, res) => {
+  try {
+    const result = await generateAllAudioFromJson(req.params.filename)
+    res.json({ success: true, ...result })
+  } catch (err) {
+    console.error('Erreur audio:', err.message)
+    res.status(500).json({ success: false, error: err.message })
+  }
 })
 
 module.exports = router
+

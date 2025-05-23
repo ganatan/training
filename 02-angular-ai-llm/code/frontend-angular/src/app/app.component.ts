5fa34f47775124f6c271b91152316f16ef7d7795
ganatan <dannyganatan@gmail.com>
2025-05-23 10:13:35 +0200

    Improve angular-ai-llm with spinner & progress bar

--- 02-angular-ai-llm/code/frontend-angular/src/app/app.component.ts
+++ 02-angular-ai-llm/code/frontend-angular/src/app/app.component.ts
@@ -24,34 +24,70 @@ export class AppComponent {
 
   chatgptDuration = 0
   claudeDuration = 0
+  chatgptProgress = 0
+  claudeProgress = 0
   useMock = environment.useMock;
 
   constructor(private personService: PersonService) { }
 
+  // loadBiography(llm: 'chatgpt' | 'claude') {
+  //   const start = performance.now()
+
+  //   if (llm === 'chatgpt') {
+  //     this.biographyChatGPT = ''
+  //     this.chatgptLoading = true
+  //   } else {
+  //     this.biographyClaude = ''
+  //     this.claudeLoading = true
+  //   }
+
+  //   this.personService
+  //     .postBiography(llm, this.name, this.length, this.style)
+  //     .subscribe((response: BiographyResponse) => {
+  //       const duration = (performance.now() - start) / 1000
+
+  //       if (llm === 'chatgpt') {
+  //         this.biographyChatGPT = response.data
+  //         this.chatgptDuration = duration
+  //         this.chatgptLoading = false
+  //       } else {
+  //         this.biographyClaude = response.data
+  //         this.claudeDuration = duration
+  //         this.claudeLoading = false
+  //       }
+  //     })
+  // }
+
   loadBiography(llm: 'chatgpt' | 'claude') {
     const start = performance.now()
+    const interval = this.startProgress(llm)
 
     if (llm === 'chatgpt') {
       this.biographyChatGPT = ''
       this.chatgptLoading = true
+      this.chatgptProgress = 0
     } else {
       this.biographyClaude = ''
       this.claudeLoading = true
+      this.claudeProgress = 0
     }
 
     this.personService
       .postBiography(llm, this.name, this.length, this.style)
       .subscribe((response: BiographyResponse) => {
         const duration = (performance.now() - start) / 1000
+        clearInterval(interval)
 
         if (llm === 'chatgpt') {
           this.biographyChatGPT = response.data
           this.chatgptDuration = duration
           this.chatgptLoading = false
+          this.chatgptProgress = 100
         } else {
           this.biographyClaude = response.data
           this.claudeDuration = duration
           this.claudeLoading = false
+          this.claudeProgress = 100
         }
       })
   }
@@ -72,5 +108,18 @@ export class AppComponent {
     this.chatgptDuration = 0
     this.claudeDuration = 0
   }
-  
+
+
+  startProgress(llm: 'chatgpt' | 'claude') {
+    let progress = 0
+    const interval = setInterval(() => {
+      progress += 5
+      if (progress >= 95) return // ne pas aller jusqu'à 100
+      if (llm === 'chatgpt') this.chatgptProgress = progress
+      else this.claudeProgress = progress
+    }, 100)
+
+    return interval
+  }
+
 }

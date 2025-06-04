6573c9cbbe7909a20f1ca231219dfab59259ccb7
ganatan <dannyganatan@gmail.com>
2025-06-04 16:21:13 +0200

    improve service on llm-connector

--- prototypes/02-llm-connector/frontend/src/app/person.ts
+++ prototypes/02-llm-connector/frontend/src/app/person.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core'
+import { Injectable, inject } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 
@@ -10,7 +10,7 @@ export interface BiographyResponse {
 
 @Injectable({ providedIn: 'root' })
 export class Person {
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient)
 
   postBiography(
     llm: string,
@@ -19,11 +19,7 @@ export class Person {
     style: string,
     type: string
   ): Observable<BiographyResponse> {
-    const url = `http://localhost:3000/api/ai/${type}/${llm}`;
-    
-    return this.http.post<BiographyResponse>(
-      url,
-      { name, length, style }
-    )
+    const url = `http://localhost:3000/api/ai/${type}/${llm}`
+    return this.http.post<BiographyResponse>(url, { name, length, style })
   }
 }

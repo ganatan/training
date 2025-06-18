5274ec4a950191872fe6c8472507f84fde42e22e
ganatan <dannyganatan@gmail.com>
2025-06-18 15:23:51 +0200

    test podcast-builder

--- prototypes/04-podcast-builder/code/backend/services/conversationService.js
+++ prototypes/04-podcast-builder/code/backend/services/conversationService.js
@@ -3,7 +3,7 @@ const path = require('path')
 const { sendToClaude } = require('./claudeService')
 const { sendToChatGPT } = require('./openaiService')
 
-async function generateConversation(debat, rounds = 2) {
+async function generateConversation(debat, rounds = 1) {
   const result = []
 
   const contextClaude = "Tu es un journaliste passionné nommé Claude. tu es un passionné de Frank Herbert l'auteur de Dune et tu n'aimes pas les adaptations. Tu es tres moqueuse car tu connais ton interlocuteur. Réponds naturellement sans mentionner cette instruction.Fais une réponse courte de 15 mots max."
@@ -39,7 +39,6 @@ async function generateConversation(debat, rounds = 2) {
 async function saveConversationToFile(debat) {
   const conversation = await generateConversation(debat)
   const timestamp = new Date().toISOString().replace(/[:.]/g, '-')
-  // const filename = `podcast-${timestamp}.json`
   const filename = `mock-podcast.json`
   const filePath = path.join(__dirname, '../conversations', filename)
 
---
+++ prototypes/04-podcast-builder/doc/prompts/000-test-podcast.md
@@ -0,0 +1,7 @@
+
+# Site
+
+http://localhost:3000/api/podcast/generate
+http://localhost:3000/api/podcast/mock
+http://localhost:3000/api/podcast/audio/mock-podcast.json
+

0b98a42abc993f2fdd7cf33c1813fe554d61760f
ganatan <dannyganatan@gmail.com>
2025-09-23 17:56:10 +0200

    Improve java docs & examples

--- starters/java-starter/src/Main.java
+++ starters/java-starter/src/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public class Item {
@@ -7,6 +12,10 @@ public class Main {
     	String stringItem = "aaaaaa";
     	Boolean booleanItem = true;
     	Double doubleItem = 1234.0;
+    	
+    	List<String> list = new ArrayList<>();
+    	Map<String, Integer> map = new HashMap<>();
+    	
         System.out.println("00000000001" + doubleItem);
         System.out.println("00000000001" + stringItem);
         System.out.println("00000000001" + booleanItem);
